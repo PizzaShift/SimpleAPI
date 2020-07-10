b8634b7598443661d5dde7b6c08caa250feedc53
eric-cabrera <52829502+eric-cabrera@users.noreply.github.com>
2020-07-10 16:00:05 -0400

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+
+
+        WeatherForecastController controller = new WeatherForecastController();
+
+        [Fact]
+        public void GetReturnMyName()
+        {
+
+            var returnValue = controller.Get("eric");
+            Assert.Equal("eric", returnValue.Value);
+
+
+        }
+    }
+}
